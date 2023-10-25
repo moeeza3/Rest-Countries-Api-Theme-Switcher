9c1219d42ac11dae2bad0fc31045acbb7f49f428
moeeza3 <moeeza3@gmail.com>
2023-10-25 22:44:43 +0500

    updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,7 @@ const Main = () => {
   
   return (
     <>
-<<<<<<< HEAD
         <section className='section py-2'>
-=======
-        <section className= 'section py-2'>
->>>>>>> c258fe4e50772bf8e6ac945732fa39133ab36855
         <div className="container-lg container-fluid ">
         <Filters />
 
